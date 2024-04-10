5a2b473dc35cd3ee0a5bd6820c9ecb9166e5d27f
Ilya Eliseev <ie@e-ise.com>
2024-04-10 14:14:48 +0300

    08-linear

--- pyml/plotting.py
+++ pyml/plotting.py
@@ -4,35 +4,53 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 
-def plot_decision_regions(X, y, classifier=None, resolution=0.02):
+def plot_decision_regions(X, y, 
+                          classifier=None, 
+                          label_y='class=',
+                          labels_text=None,
+                          cmap=None,
+                          ax=None):
     
     labels = np.unique(y)
     markers = [ 's', 'o', '^', 'v', 'x' ]
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-    color_map = ListedColormap( colors[:len(labels)] )
+    cmap = ListedColormap( colors[:len(labels)] ) if not cmap else cmap
+    ax = plt.gca() if not ax else ax 
     
-    x1_min, x2_min = X.min(0) - 1
-    x1_max, x2_max = X.max(0) + 1
+    x1_min, x2_min = X.min(0) - 0.25
+    x1_max, x2_max = X.max(0) + 0.25
     plt.xlim(x1_min, x1_max)
     plt.ylim(x2_min, x2_max)
     
     # decision areas
     if classifier:
-        x1r = np.arange(x1_min, x1_max, resolution)
-        x2r = np.arange(x2_min, x2_max, resolution)
+        x1r = np.linspace(x1_min, x1_max, 400)
+        x2r = np.linspace(x2_min, x2_max, 400)
         xx1, xx2 = np.meshgrid(x1r, x2r)
         ar = np.array([xx1.ravel(), xx2.ravel()]).T
-        Z = classifier.predict(ar).reshape(xx1.shape)
-        plt.contourf( xx1, xx2, Z, alpha=0.3, cmap=color_map)
         
-    
-    
+        if classifier.__class__.__name__=='SVC':
+            Z = classifier.decision_function(ar).reshape(xx1.shape)
+        else:
+            Z = classifier.predict(ar).reshape(xx1.shape)
+            
+        cp = ax.contour( xx1, xx2, Z, 
+                    levels=[ -1, 0, 1, ],
+                    linestyles=['--', '-', '--',],
+                    cmap=cmap)
+        ax.clabel(cp, inline=1, fontsize=10)
+        ax.contourf( xx1, xx2, Z, 
+                    alpha=0.1, 
+                    levels=[-100500, -1, 0, 1, 100500],
+                    cmap=cmap)
+
     # samples
     for k, cl in enumerate(labels):
         idx = ( y == cl )
-        plt.scatter(x=X[idx,0], y=X[idx,1],
+        label = label_y+str(labels_text[k] if labels_text else cl) if label_y else None
+        ax.scatter(x=X[idx,0], y=X[idx,1],
                     alpha=0.8, c=colors[k], marker=markers[k],
-                    label=cl, edgecolor='black')
+                    label=label, edgecolor='black')
         
         
 def plot_with_err(x, data, **kwargs):
